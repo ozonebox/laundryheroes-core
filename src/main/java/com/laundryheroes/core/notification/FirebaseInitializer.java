e290f8e692b3431737c952d9a50ce561047ce557
25 <dev@ozonebox.in>
2025-11-28 21:13:22 +0000

    New backend update

--- src/main/java/com/laundryheroes/core/notification/FirebaseInitializer.java
+++ src/main/java/com/laundryheroes/core/notification/FirebaseInitializer.java
@@ -6,19 +6,25 @@ import com.google.firebase.FirebaseOptions;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import java.io.ByteArrayInputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 @Configuration
 @RequiredArgsConstructor
 public class FirebaseInitializer {
 
     private final FcmProperties props;
-
+    private final Environment environment;
     @PostConstruct
     public void init() {
         try {
+            if (isProfileActive("ci", "test")) {
+            //log.info("Firebase initialization skipped (CI/Test profile)");
+            return;
+            }
             String serviceAccountJson = props.getServiceAccountJson();
 
             if (serviceAccountJson == null || serviceAccountJson.isBlank()) {
@@ -46,5 +52,9 @@ public class FirebaseInitializer {
                     e
             );
         }
+    }
+    private boolean isProfileActive(String... profiles) {
+        return Arrays.stream(environment.getActiveProfiles())
+            .anyMatch(p -> Arrays.asList(profiles).contains(p));
     }
 }
