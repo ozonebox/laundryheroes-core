c080da07f507ad4894b6cd753fe42784b274ae98
25 <dev@ozonebox.in>
2026-01-01 11:36:47 +0000

    orders product store update

--- src/main/java/com/laundryheroes/core/servicecatalog/ItemType.java
+++ src/main/java/com/laundryheroes/core/servicecatalog/ItemType.java
@@ -1,6 +1,7 @@
 package com.laundryheroes.core.servicecatalog;
 
 public enum ItemType {
+    // existing
     GHOTRA,
     THOB,
     THOB_WOOL,
@@ -14,4 +15,41 @@ public enum ItemType {
     BED_SHEET,
     CURTAINS_BIG,
     PILLOW,
+
+    // bedding
+    BLANKET_SINGLE,
+    BLANKET_DOUBLE,
+    DUVET_SINGLE,
+    DUVET_DOUBLE,
+    BED_COVER,
+    BED_SHEET_SINGLE,
+    BED_SHEET_DOUBLE,
+    PILLOW_COVER,
+    BED_SPREAD_COVER,
+
+    // clothing
+    SWEATER,
+    SKIRT,
+    PANTS,
+    COAT,
+    MILITARY_SUIT,
+    SCHOOL_UNIFORM,
+
+    // bath
+    TOWEL,
+    BATH_ROBE,
+    BATH_MAT,
+
+    // accessories
+    CAP,
+    TIE,
+
+    // regional items
+    SHAL_GHOTRA,
+    TOUB_COLORED,
+
+    // home
+    APRON,
+    CARPET,
+    CURTAINS_SMALL,
 }
